67181f54d0036e3abebf19fd4ed68642650d9b86
Clifford Weinmann <clifford@weinmann.africa>
2025-01-23 12:36:03 +0400

    K8s example: use recommended labels

--- fix-doc-version.sh
+++ fix-doc-version.sh
@@ -2,5 +2,5 @@
 # Fix version number in doc
 
 ver=$(cat .version)
-sed -i -e "s|image: .*$|image: docker.io/cliffordw/nginx-demo:${ver}|" k8s.yaml
+sed -i -e "s|version: .*$|version: ${ver}|" -e "s|image: .*$|image: docker.io/cliffordw/nginx-demo:${ver}|" k8s.yaml
 sed -i -e "s| docker.io/cliffordw/nginx-demo.*$| docker.io/cliffordw/nginx-demo:${ver}|" README.md
